0ca87da298885be1672d054d006e159efb41ec3c
zimwick <121416169+zimwick@users.noreply.github.com>
2025-06-05 14:02:11 -0600

    Create operand functions
    built out - + / and * functions

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 "use strict";
+
+const add = function (val1, val2) {
+  return val1 + val2;
+};
+
+const subtract = function (val1, val2) {
+  return val1 - val2;
+};
+
+const multiply = function (val1, val2) {
+  return val1 * val2;
+};
+
+const divide = function (val1, val2) {
+  return val1 / val2;
+};
+
+const operate = function (val1, operand, val2) {
+  switch (operand) {
+    case "+":
+      add(val1, val2);
+      break;
+    case "-":
+      subtract(val1, val2);
+      break;
+    case "*":
+      multiply(val1, val2);
+      break;
+    case "/":
+      divide(val1, val2);
+      break;
+    default:
+      console.log("handle error");
+  }
+};
